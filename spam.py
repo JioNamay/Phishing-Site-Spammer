650087b9be7cac7eb136bffab51cb3b499308a0e
Raphael Namay <raphael.namay@gmail.com>
2020-04-27 16:14:19 +1000

    Conformed to Pylint coding standards

--- spam.py
+++ spam.py
@@ -1,17 +1,21 @@
+'''This script sends fake email address and password combinations to the specified URL'''
+
 import random
-import requests
 import string
+import requests
 
 from credentials import first_names, last_names, passwords
 
 URL = "PHISHING SITE HERE"
 
 def random_email():
+    '''Creates and returns a random email address'''
+
     first = random.choice(first_names).lower()
     last = random.choice(last_names).lower()
 
-    random_bridge_num = random.randint(1,3)
-    random_domain_num = random.randint(1,3)
+    random_bridge_num = random.randint(1, 3)
+    random_domain_num = random.randint(1, 3)
 
     bridge = "." if random_bridge_num == 1 else "_" if random_bridge_num == 2 else ""
     domain = "@gmail.com" if random_domain_num == 1 else "@yahoo.com" if random_domain_num == 2 else "@hotmail.com"
